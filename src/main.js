b065fd1776a391c8a204d7923cd0e00829ddb4ac
Ostabo <osi.borkenhagen@gmail.com>
2022-09-27 21:23:19 +0200

    cleanup comments

--- src/main.js
+++ src/main.js
@@ -7,9 +7,6 @@ import AboutPage from "@/components/AboutPage";
 import GameField from "@/components/GameField";
 import HomePage from "@/components/HomePage";
 
-// 2. Define some routes
-// Each route should map to a component.
-// We'll talk about nested routes later.
 const routes = [
     {path: '/home', component: HomePage},
     {path: '/game', component: GameField},
@@ -17,19 +14,13 @@ const routes = [
     {path: '/:pathMatch(.*)*', redirect: '/home'},
 ]
 
-// 3. Create the router instance and pass the `routes` option
-// You can pass in additional options here, but let's
-// keep it simple for now.
 const router = createRouter({
-    // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
     history: createWebHashHistory(),
-    routes, // short for `routes: routes`
+    routes,
 })
 
-// 5. Create and mount the root instance
 const app = createApp(App)
-// Make sure to _use_ the router instance to make the
-// whole app router-aware.
+
 app.use(router)
 
 app.mount('#app')
